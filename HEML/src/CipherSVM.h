d30415426319412d4cdb621117a287d393177de0
박새롬 <saerom@wifi-131-179-5-252.host.ucla.edu>
2018-10-15 10:29:05 -0700

    fixing errors

--- HEML/src/CipherSVM.h
+++ HEML/src/CipherSVM.h
@@ -34,7 +34,7 @@ public:
 
         //for GD step and iteration
         void encLGDstep(Ciphertext encWData, Ciphertext encGrad); 
-        void encLGDiteration(Ciphertext encAtAData,Ciphertext encAbV, Ciphertext encAbH, Ciphertext* encWData, ZZX& poly, ZZX& poly2, double gamma, long sBits, long bBits, long wBits, long pBits, long aBits);
+        void encLGDiteration(Ciphertext encAtAData,Ciphertext encAbV, Ciphertext encAbH, Ciphertext encWData, ZZX& poly, ZZX& poly2, double gamma, long sBits, long bBits, long wBits, long pBits, long aBits);
         void decWData(double* wData, Ciphertext encWData, long wBits);
 
 };
